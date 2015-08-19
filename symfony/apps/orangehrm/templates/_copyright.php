4adca4a2a36df31d4587b761291b74c2d0607870
ltaeif <kortasabdellatif@gmail.com>
2015-08-19 14:28:02 +0100

    Initial commit

---
+++ symfony/apps/orangehrm/templates/_copyright.php
@@ -0,0 +1,8 @@
+<?php 
+$version = '1.0.1';
+$prodName = 'UVT RH';
+$copyrightYear = date('Y');
+
+?>
+<?php echo $prodName . ' ' . $version;?><br/>
+&copy; 2002 - <?php echo $copyrightYear;?> <a href="http://www.uvt.rnu.tn" target="_blank">UVT RH, Inc</a>. All rights reserved.
